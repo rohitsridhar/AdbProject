e7f4621a1ca9b383290eaff6f1c5219944159418
rohit <rohit@rohit-pc>
2015-11-29 11:06:41 -0600

    Reading from query file

--- AdbProject/src/adbproject/PostGIS_queries.java
+++ AdbProject/src/adbproject/PostGIS_queries.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package adbproject;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,6 +11,7 @@ import java.sql.Statement;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 
 
 
@@ -22,22 +24,29 @@ public class PostGIS_queries {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
         // TODO code application logic here
+      
         long startTime=0;
         long stopTime=0;
         Connection c = null;
        Statement stmt = null;
+       PrintStream out = new PrintStream(new FileOutputStream("src/adbproject/output.txt"));
+        System.setOut(out);
        try {
        Class.forName("org.postgresql.Driver");
          c = DriverManager
             .getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample",
             "postgres", "rohit123");
          c.setAutoCommit(false);
-         System.out.println("Opened database successfully");
-         
-         FileReader reader = new FileReader("src/adbproject/queries.txt");
+         //System.out.println("Opened database successfully");
+         double total;
+         double used;
+          
+
+
+         FileReader reader = new FileReader("src/adbproject/S_Analysis-Postgis");
          BufferedReader bufferedReader = new BufferedReader(reader);
          int i=0;
          String line;
@@ -46,10 +55,16 @@ public class PostGIS_queries {
             while ((line = bufferedReader.readLine()) != null) {
                 i++;
                 startTime = System.currentTimeMillis();
+                total  = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
                 rs = stmt.executeQuery( line );
+                used  = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
                 stopTime = System.currentTimeMillis();
                 long elapsedTime = stopTime - startTime;
-                System.out.println("Time taken for query" +i+ ":" +elapsedTime+"ms");
+                System.out.println("Time taken for query " +i+ ": " +elapsedTime+" ms");
+                System.out.print("Memory used for execution of query " +i+ ": ");
+                System.out.printf("%.2f",(used-total));
+                System.out.println(" MB");
+                System.out.println();
                 rs.close();
                 
             }        
